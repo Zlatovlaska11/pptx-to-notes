37f0b031cb6e435e9274327a7592033dca32f56b
Zlatovlaska11 <marpecqueur@gmail.com>
2024-04-25 08:59:45 +0200

    small fix

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,3 @@
-use clap::Parser;
-#[derive(Parser, Debug)]
-#[command(version = "0.9", about = "simple powerpoint to notes converter", long_about = None)]
-struct Args {
-    #[arg(short, long)]
-    file_name: String,
-
-    #[arg(short, long)]
-    create_file: bool,
-
-    #[arg(short, long)]
-    output_file_name: String,
-
-    #[arg(short, long)]
-    subject: Subject,
-}
-
-#[derive(Debug, Clone, clap::ValueEnum)]
-pub enum Subject {
-    Czech,
-    Vypoceka,
-    Chemie,
-    Dejak,
-}
-
 pub mod args_handler;
 pub mod ollama_api_wrapper;
 pub mod parse_pptx;
